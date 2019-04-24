1eb7bf908b5111ab9149a0de9d2c4ce01682e44b
Marine Gardeisen <marine.gardeisen@etu.umontpellier.fr>
2019-04-24 12:53:36 +0200

    First Commit

---
+++ src/main/java/fr/polytech/siteentraide/AltranPolytechSiteEntraideApplication.java
@@ -0,0 +1,13 @@
+package fr.polytech.siteentraide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltranPolytechSiteEntraideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltranPolytechSiteEntraideApplication.class, args);
+	}
+
+}
